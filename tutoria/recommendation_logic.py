f740a0e62369b035d143da10f2c159f0a0dfdff6
Ubuntu <ubuntu@ip-172-31-89-139.ec2.internal>
2024-05-02 21:02:17 +0000

    funciona

--- tutoria/recommendation_logic.py
+++ tutoria/recommendation_logic.py
@@ -14,27 +14,21 @@ def recommend_activities(student):
     # Regla 1: Recomendar actividades básicas si el nivel de idioma es básico
     if language_level == 'basico':
         recommended_activities.extend(Activity.objects.filter(nivel='basico'))
-        print("Actividades recomendadas para nivel básico:", recommended_activities)
 
     # Regla 2: Recomendar actividades intermedias si el nivel de idioma es intermedio
     elif language_level == 'intermedio':
         recommended_activities.extend(Activity.objects.filter(nivel='intermedio'))
-        print("Actividades recomendadas para nivel intermedio:", recommended_activities)
 
     # Regla 3: Recomendar actividades avanzadas si el nivel de idioma es avanzado
     elif language_level == 'avanzado':
         recommended_activities.extend(Activity.objects.filter(nivel='avanzado'))
-        print("Actividades recomendadas para nivel avanzado:", recommended_activities)
 
     # Regla 4: Recomendar actividades basadas en el progreso del estudiante
     for progress in user_progress:
-            # Filtrar por nivel de idioma
-            if progress.activity.nivel == language_level:
-                # Agregar la actividad si no está en la lista de recomendadas
-                if progress.activity not in recommended_activities:
-                    recommended_activities.append(progress.activity)
-
-    # Impresión de registro para verificar las actividades recomendadas finales
-    print("Actividades recomendadas finales:", recommended_activities)
+        # Filtrar por nivel de idioma
+        if progress.activity.nivel == language_level:
+            # Agregar la actividad si no está en la lista de recomendadas y no está completada
+            if progress.activity not in recommended_activities and not progress.completed:
+                recommended_activities.append(progress.activity)
 
     return recommended_activities
